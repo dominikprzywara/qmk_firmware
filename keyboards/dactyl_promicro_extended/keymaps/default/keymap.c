5e8ebc42737d9a694f31f862eb61fa0257a785a2
Dominik Przywara <dominik.przywara@gmail.com>
2023-05-05 00:56:37 +0200

    Changes to default layer

--- keyboards/dactyl_promicro_extended/keymaps/default/keymap.c
+++ keyboards/dactyl_promicro_extended/keymaps/default/keymap.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_6x7(
         KC_EQL , KC_1  ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,LOWER  ,          RAISE  ,KC_6    ,KC_7   ,KC_8   , KC_9  ,KC_0   ,KC_BSPC,
         KC_TAB , KC_Q  ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,_______,          _______,KC_J    ,KC_L   ,KC_U   , KC_Y  ,KC_SCLN,KC_MINS,
-        KC_BSPC, KC_A  ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,_______,          QWERTY ,KC_M    ,KC_N   ,KC_E   , KC_I  ,KC_O   ,LG_QUOT,
+        KC_ESC , KC_A  ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,_______,          _______,KC_M    ,KC_N   ,KC_E   , KC_I  ,KC_O   ,LG_QUOT,
         KC_LSFT, KC_Z  ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                          KC_K    ,KC_H   ,KC_COMM,KC_DOT ,CTL_SLS,KC_RSFT,
         ONE_GRV,KC_QUOT,KC_LCTL,KC_LEFT,KC_RGHT,                                           KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,TT(_LOWER),
-                                               ALT_MENU,KC_LGUI,          KC_RALT,CTL_ESC ,
+                                               ALT_MENU,KC_LGUI,          KC_RALT,KC_LCTL  ,
                                                         KC_HOME,          KC_PGUP,
                                         KC_BSPC,KC_DEL ,KC_END ,          KC_PGDN,KC_ENT  ,KC_SPC
     ),
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_6x7(
         _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,_______,          _______,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
         _______,KC_EXLM,KC_AT  ,KC_LBRC,KC_RBRC,KC_PIPE,_______,          _______,KC_UP  ,KC_P7  ,KC_P8  ,KC_P9  ,KC_PAST,KC_F12 ,
-        _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,_______,          _______,KC_DOWN,KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,_______,
+        _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,_______,          QWERTY ,KC_DOWN,KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,_______,
         _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,                          KC_AMPR,KC_P1  ,KC_P2  ,KC_P3  ,KC_PSLS,_______,
         QK_BOOT,_______,_______,_______,_______,                                          _______,KC_PDOT,KC_P0  ,KC_PEQL,_______,
                                                 _______,_______,          _______,_______,
